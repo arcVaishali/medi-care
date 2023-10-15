dfefa0f228224ad7b72d8258ed363ec325479983
vaishali <vaishalirdj14@gmail.com>
2023-10-15 14:33:26 +0530

    modified

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter , Routes , Route } from "react-router-dom"
+import IPFSUpload from './IPFSUpload';
+import IPFSUploadPinata from "./IPFSUploadPinata";
+import File from "./File";
+import IPFSPinataJSON from "./IPFSPinataJSON";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <BrowserRouter>
+   <Routes>
+    {/* <Route path = "/" element = {<IPFSUploadPinata/>}></Route> */}
+    {/* <Route path = "/" element = {<File />}></Route> */}
+    <Route path = "/" element = {<IPFSPinataJSON />}></Route>
+    <Route path="/upload-on-ipfs" element={<IPFSUpload/>}></Route>
+    <Route></Route>
+   </Routes>
+   </BrowserRouter>
   );
 }
 
